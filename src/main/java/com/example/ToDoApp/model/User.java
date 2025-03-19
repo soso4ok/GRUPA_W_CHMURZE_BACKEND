fe78ce30140413d7813583ff82ab98e8e83538d3
StraToTiQ <artur0griban123@gmail.com>
2025-03-19 11:38:38 +0100

    Add Entities to Project

---
+++ src/main/java/com/example/ToDoApp/model/User.java
@@ -0,0 +1,15 @@
+package com.example.ToDoApp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class User {
+    private String id;
+    private String email;
+    private String password;
+    private List<Task> tasks; // User's tasks
+}
