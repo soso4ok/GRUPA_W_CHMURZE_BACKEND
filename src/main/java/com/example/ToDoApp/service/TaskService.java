b9aa1b282442a497f2bac927927684882a426175
StraToTiQ <artur0griban123@gmail.com>
2025-03-21 19:59:49 +0100

    JsonDatabase: Changed (method: writeUser -> writeUsers); TaskService: Added (deleteTask, updateTaskForUser), Changed (methods: optim. getTasksForUser, addTaskToUser); UserService: Added (deleteUser), Changed (method: saveUser)

--- src/main/java/com/example/ToDoApp/service/TaskService.java
+++ src/main/java/com/example/ToDoApp/service/TaskService.java
@@ -5,7 +5,8 @@ import com.example.ToDoApp.model.User;
 import com.example.ToDoApp.repository.JsonDatabase;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
+
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
@@ -19,14 +20,51 @@ public class TaskService {
     }
 
     public List<Task> getTasksForUser(int userId) throws Exception {
-        List<User> users = jsonDatabase.readUsers();
-        Optional<User> user = users.stream().filter(u -> u.getId() == userId).findFirst();
-        return user.map(User::getTasks).orElse(List.of());
+        User user = userService.getUserById(userId);
+        return user.getTasks();
     }
 
     public void addTaskToUser(int userId, Task task) throws Exception {
-        User user = userService.getUserById(userId);
-        user.getTasks().add(task);
-        jsonDatabase.writeUser(user);
+        List<User> users = jsonDatabase.readUsers();
+        for (User user : users) {
+            if (user.getId() == userId) {
+                user.getTasks().add(task);
+                break;
+            }
+        }
+        jsonDatabase.writeUsers(users);
     }
+
+    public void deleteTaskFromUser(int userId, int taskId) throws Exception {
+        List<User> users = jsonDatabase.readUsers();
+        for (User user : users) {
+            if (user.getId() == userId) {
+                user.setTasks(
+                        user.getTasks().stream()
+                                .filter(task -> task.getId() != taskId)
+                                .collect(Collectors.toList())
+                );
+                break;
+            }
+        }
+        jsonDatabase.writeUsers(users);
+    }
+
+    public void updateTaskForUser(int userId, Task updatedTask) throws Exception {
+        List<User> users = jsonDatabase.readUsers();
+        for (User user : users) {
+            if (user.getId() == userId) {
+                List<Task> tasks = user.getTasks();
+                for (int i = 0; i < tasks.size(); i++) {
+                    if (tasks.get(i).getId() == updatedTask.getId()) {
+                        tasks.set(i, updatedTask);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        jsonDatabase.writeUsers(users);
+    }
+
 }
