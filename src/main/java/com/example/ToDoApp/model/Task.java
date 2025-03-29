25d91f1832bf9965902f91e900913b4800c9c6ea
Chiaok <bazylivillya@gmail.com>
2025-03-29 21:34:20 +0200

    Changed Task and user models

--- src/main/java/com/example/ToDoApp/model/Task.java
+++ src/main/java/com/example/ToDoApp/model/Task.java
@@ -2,11 +2,16 @@ package com.example.ToDoApp.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
+@Document("Tasks")
 public class Task {
-    private int id;
+    @Id
+    private String id;
     private String title;
     private boolean completed;
 }
--- src/main/java/com/example/ToDoApp/model/User.java
+++ src/main/java/com/example/ToDoApp/model/User.java
@@ -2,14 +2,23 @@ package com.example.ToDoApp.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Getter
 @Setter
+
+@Document("Users")
 public class User {
-    private int id;
+
+    @Id
+    private String id;
+
     private String email;
     private String password;
+    @DBRef
     private List<Task> tasks; // User's tasks
 }
